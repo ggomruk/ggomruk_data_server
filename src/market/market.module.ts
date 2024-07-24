12fa60243dc49a1d79b0b632b9f4d56dd246fa2f
eqfwcev123 <dohyeonee95@hotmail.com>
2024-07-25 00:25:29 +0800

    removed passing configService to ApiService logic as it is not required

--- src/market/market.module.ts
+++ src/market/market.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MarketSchema } from './market.schema';
-import { BinanceWebsocketService } from './binance.service';
+import { BinanceWebsocketService } from './websocket/binance.service';
 import { MarketConfig, MarketConfiglist } from 'src/config/config.interface';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MarketDataService } from './market.service';
-import { HttpModule, HttpService } from '@nestjs/axios';
-import { ApiService } from './api.service';
+import { MarketDataRepository } from './market.service';
+import { HttpModule } from '@nestjs/axios';
+import { ApiService } from './api/api.service';
 
 const getConfig = (exchangeName: string, configService: ConfigService) => {
   const exchangeConfig: MarketConfig = configService
@@ -16,31 +16,19 @@ const getConfig = (exchangeName: string, configService: ConfigService) => {
 };
 
 const serviceProviders = [
-  MarketDataService,
-  {
-    provide: ApiService,
-    useFactory: (
-      configService: ConfigService,
-      marketService: MarketDataService,
-      httpService: HttpService,
-    ) => {
-      const binanceConfig = getConfig('binance', configService);
-      if (!binanceConfig) throw new Error('Unable to find binance config');
-      return new ApiService(binanceConfig, marketService, httpService);
-    },
-    inject: [ConfigService, MarketDataService, HttpService],
-  },
+  MarketDataRepository,
+  ApiService,
   {
     provide: BinanceWebsocketService,
     useFactory: (
       configService: ConfigService,
-      marketService: MarketDataService,
+      marketService: MarketDataRepository,
     ) => {
       const binanceConfig = getConfig('binance', configService);
       if (!binanceConfig) throw new Error('Unable to find binance config');
       return new BinanceWebsocketService(binanceConfig, marketService);
     },
-    inject: [ConfigService, MarketDataService],
+    inject: [ConfigService, MarketDataRepository],
   },
 ];
 
